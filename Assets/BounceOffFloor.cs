ea7d32a8acab567bdb27d93be11e6e76bd1933c5
Mike Martens <mam18@case.edu>
2019-09-08 10:21:21 -0400

    Minor changes.
    This version was installed on the HoloLens and ran successfully.

--- Assets/BounceOffFloor.cs
+++ Assets/BounceOffFloor.cs
@@ -7,7 +7,7 @@ public class BounceOffFloor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Called BounceOffFloor.Start()");
+      
     }
 
     // Update is called once per frame
@@ -16,17 +16,28 @@ public class BounceOffFloor : MonoBehaviour
 
     }
 
-    void OnTriggerEnter(Collider other)
+   
+    // OnTriggerEnter is called whenever a collision is detected between 
+    // this object (which is the BouncingBall in this example) and another ojbect.
+       void OnTriggerEnter(Collider other)
     {
+
+        Vector3 v3Position;
+        v3Position = this.transform.position;
+        
         Rigidbody rb = this.GetComponent<Rigidbody>();
         Vector3 v3Velocity = rb.velocity;
+       
+        print("Detected collision between " + this.name + " and " + other.name + "\n" +
+           "Position" + v3Position.ToString("F4") + " and " + "Velocity" + v3Velocity.ToString("F4") );
+
+        // Reverse the vertical (y-axis) velocity to simulate an elastic collisions
         float Vx = v3Velocity.x;
         float Vy = v3Velocity.y;
         float Vz = v3Velocity.z;
-
-        print("Detected collision between " + this.name + " and " + other.name);
-        print("Velocity" + v3Velocity);
         v3Velocity = new Vector3(Vx, -Vy, Vz);
         rb.velocity = v3Velocity;
+
+        
     }
 }
